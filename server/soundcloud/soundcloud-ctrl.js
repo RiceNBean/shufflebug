c2caf4cd8dfa30225e29508d48fddf7e9ffe9f23
Calvin Ton <cton43@yahoo.com>
2016-08-30 17:47:27 -0700

    [refactor] changed exports format, removed API/DB keys, filtered API request

--- server/soundcloud/soundcloud-ctrl.js
+++ server/soundcloud/soundcloud-ctrl.js
@@ -1,13 +1,16 @@
 var soundcloudModel = require('./soundcloud-model.js');
 
-var search = function (req, res) {
-	console.log('inside soundcloud-ctrl.js search controller');
-	soundcloudModel.search(req.query)
-	.then(function(data) {
-		res.status(200).json(data);
-	});
-}
-
 module.exports = {
-	search: search
+
+	search: function (req, res) {
+		console.log('inside soundcloud-ctrl.js search controller');
+		soundcloudModel.search(req.query)
+		.then(function(data) {
+			results = data.map(function(song) {
+				return {songURL: song.permalink_url, title: song.title, duration: song.duration}
+			});
+			res.status(200).json(results);
+		});
+	}
+
 }
