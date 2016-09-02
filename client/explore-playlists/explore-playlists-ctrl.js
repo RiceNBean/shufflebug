48e1737ddfedd76f0049f979086b5dea992daee2
davidkim310 <davidkim310@gmail.com>
2016-09-01 21:23:44 -0700

    [client-feat] explore feature loads all databases, add routing for playlist endpoints

--- client/explore-playlists/explore-playlists-ctrl.js
+++ client/explore-playlists/explore-playlists-ctrl.js
@@ -1,7 +1,20 @@
-angular.module('app.explore', [])
-.controller('ExploreCtrl', ExploreCtrl);
-function ExploreCtrl(explore){
-  var vm = this;
+angular.module('app.explore', ['ngCookies'])
+.controller('ExploreCtrl', function($scope, Explore, $cookies, $state){
+    $scope.data;
+    //on click we should go to a new endpoint
+    $scope.redirectPlaylist = function(_id){
+      //grab that id
+      //$cookies.get(_id);
+      //we concat the playlistID to 'playlist' endpoint to access each playlist
+      $state.go('playlist/' + _id)
+    };
+    //get data from our database
+    Explore.getPlaylists()
+    .then(function(data){
+      //assign that data to $scope.data so we can use in html
+      $scope.data = data;
+    }).catch(function(error){
+      console.log(error);
+    });
 
-  return vm;
-}
+});
--- client/explore-playlists/explore-playlists-factory.js
+++ client/explore-playlists/explore-playlists-factory.js
@@ -1,22 +1,23 @@
 angular.module('app.explore')
-.factory("explore", getPlaylists);
-function getPlaylists(data){
-  return $http({
+.factory("Explore", function($http){
+//getPlaylists to private
+  return {
+    getPlaylists: getPlaylists
+  };
+//our http request to get all data from this endpoint
+  function getPlaylists(data){
+    return $http({
       method: 'GET',
       url: '/db/playlists/getAll',
       params: {
-        playlistID: playlistID,
-        playlistTitle: playlistTitle,
-        playlistImg: PlaylistImg,
-        description: description
+        data: data
       }
-    }).then(function(res){
-      return res.data;
+      //promise to show successful GET request
+    }).then(function(success){
+      return success.data;
+      //give us error if error
     }).catch(function(error){
-      console.log("error");
-    })
-    return {
-      getPlaylists:getPlaylists
-    }
-
-}
+      console.log("error retrieving data: ", error);
+    });
+  }
+});
