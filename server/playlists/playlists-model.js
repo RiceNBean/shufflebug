d8e33373d9b225e9238efef9576ef858d33b4baa
Alberto M Esquivias <alberto.m.esquivias@gmail.com>
2016-09-02 14:17:23 -0700

    [server-feat] Adds createdAt field to playlist model, field will expire document after number of seconds have passed

--- server/playlists/playlists-model.js
+++ server/playlists/playlists-model.js
@@ -10,11 +10,12 @@ mongoose.connect(process.env.MONGODB_URI);
 //playlist schema model
 var PlaylistSchema = new mongoose.Schema({
 	//all fields are given a name and a type
+	//syntax for adding a field with a default, required && unique value
 	name: { type: String, required: true, unique: true },
 	description: { type: String, required: true },
 	limit: { type: Number, default: 25 },
-	//syntax for adding a field with a default value
-	expiration: { type: Number, default: 604800 },
+	//experation, will expire after num of seconds in a week
+	createdAt: { type: Date, expires: 604800, default: Date.now },
 	score: Number,
 	songs: [{
 		title: String,
