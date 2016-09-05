ca527e3126a97117c2deab1b6ab585267c7e5d38
Calvin Ton <cton43@yahoo.com>
2016-09-05 15:54:32 -0700

    [refactor] cleaned up code to be consistent

--- client/player/player-factory.js
+++ client/player/player-factory.js
@@ -1,18 +1,18 @@
 angular.module('app.player')
-.factory("Player", Player);
+.factory('Player', Player);
 
 function Player($http) {
 
-  var currentSong = "https://soundcloud.com/arigasu/spring-time-yiruma";
+  var currentSong = 'https://soundcloud.com/arigasu/spring-time-yiruma';
 
   function setCurrent(songURL) {
-    console.log("inside player-factory.js setCurrent", songURL);
+    console.log('inside player-factory.js setCurrent', songURL);
     currentSong = songURL;
     return;
   }
 
   function getCurrent() {
-    console.log("inside player-factory.js getCurrent");
+    console.log('inside player-factory.js getCurrent');
     return currentSong;
   }
   
